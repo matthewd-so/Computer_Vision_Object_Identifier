a3cf6a6f9f2a1701833a4df2f1e89fd66813fd18
matthewd-so <61816265+matthewd-so@users.noreply.github.com>
2023-05-09 15:51:03 -0400

    Add files via upload

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div style={{ backgroundColor: 'blue', padding: '20px' }}>
+    <div style={{ textAlign: 'center', marginTop: '20px', backgroundColor: 'blue'}}>
+      <h1 style = {{color: 'white' }} >Welcome to my Object Detection App!</h1>
+    </div>
+    </div>
+  );
+}
+
+export default Header;
